5e2b03a145cdc5edf29656ff97599de19a47c42b
Weiya Zhu <weiyazhu@student.ubc.ca>
2023-11-21 13:26:45 -0800

    Added comments to the app.R

--- app.R
+++ app.R
@@ -1,33 +1,50 @@
-# Load libraries
+# load al libraries
 library(shiny)
 library(tidyverse)
 library(DT)
 library(datateachr)
 
-# Load the dataset
+# load the vancouver_trees dataset
 trees_dataset <- datateachr::vancouver_trees
 
-# Define UI for application
+# define User In Interface for the application
 ui <- fluidPage(
   titlePanel("Vancouver Trees Explorer App"),
+  # define the layout of sidebar
   sidebarLayout(
     sidebarPanel(
+      # a feature which contains an image at the top
       img(src = "forest.png", width = "420px", height = "300px"),
+
+      # a feature that creates a dropdown menu for the user to select the tree species for histogram
       selectInput("species", "Select Histogram Tree Species:",
                   choices = unique(trees_dataset$species_name),
                   selected = unique(trees_dataset$species_name)[1]),
+
+      # a feature that creates a dropdown menu for the user to select a numeric variable for histogram
       selectInput("variable", "Select Histogram Variable:",
                   choices = names(trees_dataset %>% select_if(is.numeric)),
                   selected = "height_range_id"),
+
+      # a feature that provides users with a slider interface that allows
+      # them to select a bin width within a specified range
       sliderInput("binwidth", "Select Histogram Bin Width:",
                   min = 1, max = 50, value = 1),
+
+      # a feature that creates a dropdown menu for the user to select a numeric variable for x_axis of the scatterplot
       selectInput("x_variable", "Select Scatterplot x_axis Variable:",
                   choices = names(trees_dataset %>% select_if(is.numeric)),
                   selected = "height_range_id"),
+
+      # a feature that creates a dropdown menu for the user to select a numeric variable for y_axis of the scatterplot
       selectInput("y_variable", "Select Scatterplot y_axis Variable:",
                   choices = names(trees_dataset %>% select_if(is.numeric)),
                   selected = "diameter"),
+
+      # a feature allows users to download csv for selected data table
       downloadButton("downloadData", "Download Filtered Data"),
+
+      # a feature allows users to select one or multiple tree species for data table
       checkboxGroupInput("selected_species", "Select Tree Species for Data Table:",
                          choices = unique(trees_dataset$species_name),
                          selected = unique(trees_dataset$species_name)[1],
@@ -35,12 +52,13 @@ ui <- fluidPage(
     ),
     mainPanel(
       tabsetPanel(
+        # include three different tab panels which generate histograms, scatter plots and data tables respectively
         tabPanel("Histogram",
                  plotOutput("histogram", height="600px"),
                  h4("Total Number of Trees of This Species:"),
                  textOutput("total_results")),
         tabPanel("Scatter Plot",
-                 plotOutput("scatter_plot")),
+                 plotOutput("scatter_plot", height="600px")),
         tabPanel("Data Table",
                  h4("Selected Tree Species Data Table"),
                  DTOutput("data_table"))
@@ -52,19 +70,21 @@ ui <- fluidPage(
 # Define server logic
 server <- function(input, output) {
 
-  # Reactive expression for the selected data
+  # a reactive expression for the filtered data
   filtered_data <- reactive({
     trees_dataset %>%
       filter(species_name == input$species)
   })
 
+  # a reactive expression for the checkbox data
   selected_checkbox_data <- reactive({
     trees_dataset %>%
       filter(species_name %in% input$selected_species)
   })
 
-  # Generate a histogram
+  # generate a histogram
   output$histogram <- renderPlot({
+    # filter out rows with missing values or inf values
     data_filtered <- filtered_data() %>%
       filter(!is.na(!!sym(input$variable)), !is.infinite(!!sym(input$variable)))
 
@@ -75,19 +95,19 @@ server <- function(input, output) {
       theme_bw()
   })
 
-  # Display total number of results
+  # display the total number of trees
   output$total_results <- renderText({
     paste(nrow(filtered_data()), "trees")
   })
 
-  # Display data table with DT
+  # display the data table using DT packages
   output$data_table <- renderDT({
     datatable(selected_checkbox_data (), options = list(pageLength = 10))
   })
 
-  # Scatter Plot
+  # generate a scatter plot
   output$scatter_plot <- renderPlot({
-    # Filter out rows with missing values in x and y variables
+    # filter out rows with missing values in x and y variables
     filtered_data <- filtered_data() %>%
       filter(!is.na(!!sym(input$x_variable)), !is.na(!!sym(input$y_variable)))
 
@@ -98,8 +118,7 @@ server <- function(input, output) {
       theme_bw()
   })
 
-
-  # Allow user to download csv
+  # allow user to download csv
   output$downloadData <- downloadHandler(
     filename = function() {
       paste("filtered_data_", Sys.Date(), ".csv", sep = "")
